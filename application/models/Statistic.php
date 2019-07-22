de81aec631c984f9bff5eaa52a02a3c9db0d9c1a
Andrey <andreylashenko@mail.ru>
2019-07-22 14:18:37 +0300

    clear records

--- application/models/Statistic.php
+++ application/models/Statistic.php
@@ -110,31 +110,4 @@ class Statistic extends Model
 
         return  $return_arr;
     }
-
-    public function deleteTodayRecords(array $phones)
-    {
-        $dateStart = Date('Y-m-d H:i:s', strtotime('today midnight'));
-        $dateEnd = Date('Y-m-d H:i:s', time());
-        $deletedRecords = [];
-        $sql_condition = " calldate >= '".$dateStart."' AND calldate < '".$dateEnd."'";
-
-        $sql = "SELECT * FROM cdr
-                WHERE ".$sql_condition." 
-                AND disposition LIKE 'ANSWERED' 
-                AND channel NOT LIKE 'Local/%'";
-
-        foreach ($this->db->row($sql) as $row) {
-            if(!in_array($row['dst'], $phones)) {
-                $this->db->delete('DELETE FROM cdr WHERE id = :id', ["id" => $row['id']]);
-                $deletedRecords[] = $row['dst'];
-            }
-        }
-
-        $return_arr = array(
-            'status' 	=> 'success',
-            'data' 	=> $deletedRecords
-        );
-
-        return $return_arr;
-    }
 }
